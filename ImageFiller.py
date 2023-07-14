008e48a1a010f80db4795bc4a9b659a5a556d456
Daniel Beltejar <danielbeltejar@gmail.com>
2023-07-14 19:27:53 +0200

    First commit.

---
+++ ImageFiller.py
@@ -0,0 +1,113 @@
+from datetime import datetime
+from time import sleep
+
+import requests
+from PIL import Image
+
+
+class ImageFiller:
+    def __init__(self, image_path, pokemons, temperature_ranges, api_key):
+        self.image = Image.open(image_path)
+        self.image = self.image.convert("RGBA")  # Convert to RGBA mode for transparency support
+        self.width, self.height = self.image.size
+        self.filled_image = Image.new("RGBA", (self.width, self.height), (0, 0, 0, 0))
+        self.filled_image.paste(self.image, (0, 0))
+        self.pokemons = pokemons
+        self.temperature_ranges = temperature_ranges
+        self.api_key = api_key
+
+    def bucket_fill(self, seed_point, fill_color):
+        target_color = self.filled_image.getpixel(seed_point)
+
+        if target_color == fill_color:
+            return
+
+        pixels_to_check = [seed_point]
+        processed_pixels = set()
+
+        while pixels_to_check:
+            x, y = pixels_to_check.pop()
+
+            if (
+                    0 <= x < self.width and
+                    0 <= y < self.height and
+                    self.filled_image.getpixel((x, y)) == target_color and
+                    (x, y) not in processed_pixels
+            ):
+                self.filled_image.putpixel((x, y), fill_color)
+                processed_pixels.add((x, y))
+
+                pixels_to_check.extend(
+                    [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+                )
+
+    def fill_image(self):
+        from main import provincias
+
+        base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+        for provincia, coords in provincias.items():
+            x, y = map(int, coords.split(","))
+            seed_point = (x, y)
+
+            params = {
+                "q": provincia + ",es",
+                "appid": self.api_key,
+                "units": "metric",
+                "dt": int(datetime.now().replace(hour=12, minute=0, second=0).timestamp())
+            }
+
+            response = requests.get(base_url, params=params)
+            data = response.json()
+
+            if response.status_code == 200:
+                estado_tiempo = data["weather"][0]["description"]
+                temperatura = data["main"]["temp"]
+
+                if temperatura >= 36.0:
+                    pokemon_asociado = "Charizard"
+                elif temperatura >= 34.0:
+                    pokemon_asociado = "Charmeleon"
+                elif temperatura >= 32.0:
+                    pokemon_asociado = "Charmander"
+                elif temperatura <= 4.0:
+                    pokemon_asociado = "Regice"
+                else:
+                    pokemon_asociado = self.pokemons.get(estado_tiempo, "Castform")
+                pokemon_asociado = pokemon_asociado.lower()
+
+                fill_color = None
+                for temp_range in self.temperature_ranges:
+                    if temp_range["range"][0] <= temperatura < temp_range["range"][1]:
+                        fill_color = temp_range["color"]
+                        break
+
+                if fill_color is not None:
+                    print(f"Provincia: {provincia}")
+                    print(f"Estado del tiempo: {estado_tiempo}")
+                    print(f"Pokémon asociado: {pokemon_asociado}")
+                    print(f"Temperatura: {temperatura}°C")
+                    print(f"Color: {fill_color}")
+                    self.bucket_fill(seed_point, fill_color)
+
+                    # Load and paste the Pokémon sprite onto the filled_image
+                    pokemon_image = Image.open(f"images/pokemons/{pokemon_asociado}.png")
+                    pokemon_image = pokemon_image.resize((110, 110))  # Adjust the size as needed
+                    # Get the dimensions of the Pokémon image
+                    pokemon_width, pokemon_height = pokemon_image.size
+
+                    # Calculate the coordinates for pasting the Pokémon image from the center
+                    paste_x = seed_point[0] - pokemon_width // 2
+                    paste_y = seed_point[1] - pokemon_height // 2
+
+                    # Paste the resized Pokémon sprite onto the filled_image at the adjusted coordinates
+                    self.filled_image.paste(pokemon_image, (paste_x, paste_y), pokemon_image)
+
+                    print()
+                else:
+                    print(f"No se encontró un color adecuado para la temperatura {temperatura}°C")
+            sleep(0.05)
+
+    def save_image(self, output_path):
+        self.filled_image.convert("RGB").save(output_path, "JPEG", quality=90)
+        self.filled_image.show()
---
+++ main.py
@@ -0,0 +1,133 @@
+import uuid
+from datetime import datetime
+
+from ImageFiller import ImageFiller
+
+# Obtén tu clave de API de OpenWeatherMap
+api_key = "6b5c8c87c1b593c6acef24500942bcc5"
+
+# Diccionario de asignación de Pokémon a estados del tiempo
+pokemons = {
+    "clear sky": "Solrock",
+    "clouds": "Togekiss",
+    "few clouds": "Castform",
+    "scattered clouds": "Castform",
+    "broken clouds": "Castform",
+    "overcast clouds": "Togekiss",
+    "smoke": "Weezing",
+    "haze": "Sandslash",
+    "dust": "Sandslash",
+    "sand": "Sandslash",
+    "ash": "Sandslash",
+    "squalls": "Castform",
+    "tornado": "Thundurus",
+    "rain": "Castform_rainy",
+    "drizzle": "Castform_rainy",
+    "thunderstorm": "Zapdos",
+    "snow": "Articuno",
+    "mist": "Articuno",
+    "fog": "Gengar",
+    "hail": "Avalugg"
+}
+
+# Temperature ranges and associated colors
+temperature_ranges = [
+    {"range": (-100.0, 0.0), "color": (135, 206, 250, 255)},  # Light Blue
+    {"range": (0.0, 2.5), "color": (163, 200, 225, 255)},  # Sky Blue
+    {"range": (2.5, 5.0), "color": (173, 216, 230, 255)},  # Powder Blue
+    {"range": (5.0, 7.5), "color": (176, 223, 228, 255)},  # Light Steel Blue
+    {"range": (7.5, 10.0), "color": (176, 224, 230, 255)},  # Light Cyan
+    {"range": (10.0, 12.5), "color": (199, 234, 234, 255)},  # Aquamarine
+    {"range": (12.5, 15.0), "color": (224, 255, 255, 255)},  # Pale Turquoise
+    {"range": (15.0, 17.5), "color": (185, 242, 159, 255)},  # Light Green
+    {"range": (17.5, 20.0), "color": (144, 238, 144, 255)},  # Lime Green
+    {"range": (20.0, 22.5), "color": (0, 255, 0, 255)},  # Lime
+    {"range": (22.5, 25.0), "color": (50, 205, 50, 255)},  # Lime Green (Web)
+    {"range": (25.0, 27.5), "color": (255, 255, 0, 255)},  # Yellow
+    {"range": (27.5, 30.0), "color": (255, 215, 0, 255)},  # Gold
+    {"range": (30.0, 32.5), "color": (255, 165, 0, 255)},  # Orange
+    {"range": (32.5, 35.0), "color": (255, 140, 0, 255)},  # Dark Orange
+    {"range": (35.0, 37.5), "color": (255, 69, 0, 255)},  # Orange Red
+    {"range": (37.5, 40.0), "color": (255, 0, 0, 255)},  # Red
+    {"range": (40.0, 42.5), "color": (139, 0, 0, 255)},  # Dark Red
+    {"range": (42.5, 45.0), "color": (128, 0, 0, 255)},  # Maroon
+    {"range": (45.0, 100.0), "color": (128, 0, 0, 255)}  # Maroon
+]
+
+# Lista de provincias de España
+provincias = {
+    "Ceuta": "523,1314",
+    "Melilla": "806,1351",
+    "Ibiza": "1256,864",
+    "Formentera": "1254,924",
+    "Menorca, Illes Balears": "1488,735",
+    "Arrecife, Gran Canaria": "1514,1158",
+    "Puerto del Rosario, Gran Canaria": "1449,1281",
+    "Valverde, Santa Cruz de Tenerife": "1054,1329",
+    "San Sebastian de La Gomera, Santa Cruz de Tenerife": "1152,1270",
+    "Santa Cruz de La Palma, Santa Cruz de Tenerife": "1079,1179",
+    "Álava": "826,388",
+    "Albacete": "888,934",
+    "Alicante": "1025,970",
+    "Almería": "859,1142",
+    "Asturias": "491,324",
+    "Ávila": "593,687",
+    "Badajoz": "452,943",
+    "Barcelona": "1280,541",
+    "Burgos": "726,464",
+    "Cáceres": "456,794",
+    "Cádiz": "477,1227",
+    "Cantabria": "673,346",
+    "Castellón": "1072,725",
+    "Ciudad Real": "703,904",
+    "Córdoba": "598,1028",
+    "Cuenca": "858,775",
+    "Gerona": "1352,477",
+    "Granada": "736,1140",
+    "Guadalajara": "821,663",
+    "Guipúzcoa": "858,347",
+    "Huelva": "372,1072",
+    "Huesca": "1065,462",
+    "Illes Balears": "1395,798",
+    "Jaén": "735,1025",
+    "La Coruña": "252,330",
+    "La Rioja": "834,462",
+    "Las Palmas": "1336,1300",
+    "León": "506,418",
+    "Lérida": "1177,499",
+    "Lugo": "350,365",
+    "Madrid": "716,695",
+    "Málaga": "605,1180",
+    "Murcia": "946,1030",
+    "Navarra": "913,406",
+    "Orense": "333,470",
+    "Palencia": "630,444",
+    "Pontevedra": "245,424",
+    "Salamanca": "472,648",
+    "Segovia": "684,609",
+    "Sevilla": "494,1097",
+    "Soria": "826,546",
+    "Tarragona": "1164,608",
+    "Santa Cruz de Tenerife": "1225,1247",
+    "Teruel": "993,691",
+    "Toledo": "657,787",
+    "Valencia": "1023,853",
+    "Valladolid": "603,560",
+    "Bizkaia": "808,333",
+    "Zamora": "489,536",
+    "Zaragoza": "966,554"
+}
+
+# URL base de la API de OpenWeatherMap
+base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+# Crear una instancia de ImageFiller
+image_filler = ImageFiller("images/spain.png", pokemons, temperature_ranges, api_key)
+
+# Llenar la imagen
+image_filler.fill_image()
+
+# Guardar y mostrar la imagen modificada
+image_filler.save_image(f"images/output/spain/{datetime.now().strftime('%Y%m%d')}.{uuid.uuid4()}.jpg")
+
+exit()
