f1cdfbb8e9b66033103b438bfda19ec723fdd8c9
Daniel Beltejar <danielbeltejar@gmail.com>
2023-08-26 13:09:39 +0200

    Added healths endpoints for the webservices.

---
+++ webservice-date/src/main/java/com/example/polemonweathermap/controllers/HealthController.java
@@ -0,0 +1,17 @@
+package com.example.polemonweathermap.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthz")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("OK");
+    }
+}
