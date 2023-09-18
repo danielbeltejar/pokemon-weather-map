9b698b706c4ea26056d8b00004b4b7bda20d51d6
Daniel Beltejar <danielbeltejar@gmail.com>
2023-09-18 02:58:38 +0200

    UX improvements.

--- client/public/assets/scripts/map_zoom.js
+++ client/public/assets/scripts/map_zoom.js
@@ -1,15 +1,18 @@
 
 const zoomedContainer = document.getElementById('zoomed-image-container');
+const bodyBackground = document.getElementById("background")
 
 function zoomInImage(selectedDiv) {
     const origImg = selectedDiv.querySelector('img');
     const clonedImg = origImg.cloneNode(true);
 
     zoomedContainer.appendChild(clonedImg);
+    bodyBackground.classList.remove("lg:bg-gray-200")
 }
 
 zoomedContainer.addEventListener('click', function () {
     while (zoomedContainer.firstChild) {
         zoomedContainer.removeChild(zoomedContainer.firstChild);
+        bodyBackground.classList.add("lg:bg-gray-200")
     }
 });
